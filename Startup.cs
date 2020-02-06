7627478ca259d39d0cadb72d520a0b6a6666e3e8
Kurt Duyck <kurt.duyck@vives.be>
2020-02-06 14:55:50 +0100

    Initial commit
    	new file:   .gitignore
    	new file:   Program.cs
    	new file:   Properties/launchSettings.json
    	new file:   Startup.cs
    	new file:   dotnet-lab.csproj

---
+++ Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace dotnet_lab
+{
+    public class Startup
+    {
+        private string _moviesApiKey = null;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _moviesApiKey = Configuration["Movies:ServiceApiKey"];
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync(" - ");
+                var result = string.IsNullOrEmpty(_moviesApiKey) ? "Null" : "Not Null";
+                await context.Response.WriteAsync($"Secret is {result}");
+            });
+        }
+    }
+}
