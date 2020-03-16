8677921a17a1f02f5f9a526deb42ae946f1b6214
Kurt Duyck <kurt.duyck@vives.be>
2020-03-16 14:59:32 +0100

    Lab to store DB keys in secret.json store
    	new file:   CHANGELOG.md
    	new file:   README.md
    	modified:   Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -13,6 +14,8 @@ namespace dotnet_lab
     public class Startup
     {
         private string _moviesApiKey = null;
+	private string _connection = null;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -24,8 +27,11 @@ namespace dotnet_lab
         public void ConfigureServices(IServiceCollection services)
         {
             _moviesApiKey = Configuration["Movies:ServiceApiKey"];
-        }
+            var builder = new SqlConnectionStringBuilder(Configuration.GetConnectionString("Movies"));
+            builder.Password = Configuration["DbPassword"];
+            _connection = builder.ConnectionString;
 
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -39,7 +45,11 @@ namespace dotnet_lab
                 await context.Response.WriteAsync("Hello World!");
                 await context.Response.WriteAsync(" - ");
                 var result = string.IsNullOrEmpty(_moviesApiKey) ? "Null" : "Not Null";
+                await context.Response.WriteAsync(" - ");
                 await context.Response.WriteAsync($"Secret is {result}");
+                await context.Response.WriteAsync(" - ");
+                await context.Response.WriteAsync($"DB Connection: {_connection}");
+                await context.Response.WriteAsync(" - ");
             });
         }
     }
